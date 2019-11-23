4a6854808001b23cee719826bb3d909e7af372c0
wupeiqun <wupeiqun@xdf.cn>
2019-11-23 11:47:37 +0800

    zk

---
+++ zookeeper/src/main/java/com/zk/ZookeeperApplication.java
@@ -0,0 +1,19 @@
+package com.zk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: victor
+ * @email: wupeiqun@xdf.cn
+ * @create: 2019-11-22 11:44
+ */
+@SpringBootApplication
+public class ZookeeperApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ZookeeperApplication.class);
+  }
+
+}
