44468dc687d32f220ae661dbaebb4d081e4a19e4
wupeiqun <wupeiqun@xdf.cn>
2021-03-06 11:37:10 +0800

    img

---
+++ designpattern/src/main/java/com/design/proxy/d/jdk/Test.java
@@ -0,0 +1,10 @@
+package proxy.d.jdk;
+
+/**
+ * @description:
+ * @author: victor
+ * @email: wupeiqun@xdf.cn
+ * @create: 2021-01-16 16:51
+ */
+public class Test {
+}
---
+++ mylog/src/main/java/com/mylog/service/StudentService.java
@@ -0,0 +1,32 @@
+package com.log.service;
+
+import lombok.extern.slf4j.Slf4j;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Service;
+
+/**
+ * @description:
+ * @author: victor
+ * @email: wupeiqun@xdf.cn
+ * @create: 2021-02-04 11:02
+ */
+@Service
+@Slf4j(topic = "StudentService")
+public class StudentService {
+
+  public Map<String, Object> getStuInfo(String name) {
+
+    log.info("StudentService getStuInfo  param:{}", name);
+
+    Map<String, Object> m = new HashMap<>();
+
+    m.put("name", name);
+    m.put("age", "19");
+    m.put("hight", "174cm");
+    m.put("wight", "62kg");
+
+    return m;
+  }
+
+}
